e4f1e9fad82cc8cb1bcf67dbad384bf6b65a5248
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2022-12-10 16:55:26 +0100

    new write endpoints

--- R/write.R
+++ R/write.R
@@ -1,5 +1,5 @@
+# Pages -------------------------------------------------------------------
 
-#
 #' Title
 #'
 #' @param title
@@ -22,3 +22,48 @@ create_page <- function(title, ...) {
   roamR::req_roam(data = data, route = "write", ...)
 }
 
+#' Title
+#'
+#' @param title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+update_page <- function(uid, title, ...) {
+  data <- list(
+    action = "update-page",
+    page = list(
+      uid = as.character(uid), # TODO: checks!
+      title = as.character(title)
+    )
+  )
+
+  ## Handling
+  # Bad Request if page name is non-unique!!
+
+  roamR::req_roam(data = data, route = "write", ...)
+}
+
+
+#' Title
+#'
+#' @param uid
+#'
+#' @return
+#' @export
+#'
+#' @examples
+delete_page <- function(uid, ...) {
+  data <- list(
+    action = "delete-page",
+    page = list(
+      uid = as.character(uid) # TODO: checks!
+    )
+  )
+
+  ## Handling
+  # Bad Request if page name is non-unique!!
+
+  roamR::req_roam(data = data, route = "write", ...)
+}
